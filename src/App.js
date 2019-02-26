014d896a9e2ddf46cb4aa06ffa5e036fdca96fd3
nishi1612 <doshinishi1612@gmail.com>
2019-02-26 22:44:03 +0530

    Second update

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import React, { Component } from 'react';
+import './App.css';
+import fire from './config/fire';
+import Home from './Home';
+import Login from './Login';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+  render() {
+    return (
+     <div>
+       <Login />
+      </div>
+    //  { {this.state.user ? ( <Home/>) : (<Login />)}</div> */}
+    );
+  }
+}
+
+ export default App;
